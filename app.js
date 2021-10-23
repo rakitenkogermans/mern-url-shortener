bbb6722cf2cad0cba7018da93087d62ef8bc63bf
rakitenkogermans <rakitenko.germans@gmail.com>
2021-10-23 15:43:37 +0300

    Redirect route. From short url redirect to the original url.

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ app.use(express.json({extended: true}));
 //Register our routes
 app.use('/api/auth', require('./routes/auth.routes'));
 app.use('/api/link', require('./routes/link.routes'));
+app.use('/t', require('./routes/redirect.routes'));
 
 const PORT = config.get('port') || 5000;
 
