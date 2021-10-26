a2ec834baab81547a5e5b2f64989b9408d59ed9a
rakitenkogermans <rakitenko.germans@gmail.com>
2021-10-26 13:52:07 +0300

    heroku deploy

--- app.js
+++ app.js
@@ -15,19 +15,19 @@ app.use('/api/link', require('./routes/link.routes'));
 app.use('/t', require('./routes/redirect.routes'));
 
 if (process.env.NODE_ENV === 'production') {
-    app.use('/', express.static(path.join(__dirname, 'client', 'build')));
+    app.use(express.static('client/build'));
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
+    // app.get('*', (req, res) => {
+    //     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    // });
 }
 
-const PORT = config.get('port') || 5000;
+const PORT = config.get('port') || 8080;
 
 async function start() {
     try {
         //If all is okey, connect to mongodb
-        await mongoose.connect(config.get('mongoUri'), {});
+        await mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/mernUrlShortener', {});
         app.listen(PORT, () => console.log('App started on port', PORT));
     } catch (e) {
         //Exit process if something went wrong
